13e5557a6335cf761e98e2ec35facdb8214ccc03
Evgeniy <zelevzel@ya.ru>
2020-10-02 03:02:28 +0300

    feat:complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
+// exports.min = function min (array) {
+
+//     if (!array || !array.length) {
+//         return 0;
+//     }
+
+//     let minNumber = array[0];
+
+//     array.forEach((value, index) => {
+
+
+//         if (minNumber > value) {
+//             minNumber = value;      
+//         }     
+//     });
+//     return minNumber;
+// }
+
+
 exports.min = function min (array) {
-  return 0;
+    if (!array || !array.length) {
+    return 0;}
+
+    array.sort((a,b) => a-b);
+    return array[0];
 }
 
+
 exports.max = function max (array) {
-  return 0;
+    if (!array || !array.length) {
+        return 0;
+    }
+    
+    array.sort((a,b) => a-b);
+    return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (!array || !array.length) {
+        return 0;
+    }
+
+    let i = array.reduce((acc, item)=>acc + item);
+  return (i / array.length);
 }
